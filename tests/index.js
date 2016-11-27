2da3f2b790158cdc94e0f68ac9b5aea771005957
Mattia Occhiuto <occhiutomattia@gmail.com>
2016-11-27 09:25:11 +0100

    Add tests

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,46 @@
-var actions = require('./actions');
+'use strict';
 
-console.log(actions);
+import { expect } from 'chai';
+import actionsCreatorFactory from '../src';
+
+describe('Comprehension Test', function () {
+    describe('Comprehensions function tests', function () {
+        const actions = [
+            'TEST_NO_PAYLOAD',
+            'TEST_PAYLOAD_ALL',
+            {
+                type: 'TEST_PAYLOAD_FUNCTION',
+                payload: (data) => data + ' test',
+            },
+        ];
+
+        const functionsCreated = actionsCreatorFactory(actions);
+
+        it('Generate a function for each configured actions', function () {
+            expect(functionsCreated['testPayloadFunction']).to.be.a('function');
+            expect(functionsCreated['testPayloadAll']).to.be.a('function');
+            expect(functionsCreated['testNoPayload']).to.be.a('function');
+        });
+
+        it('No payloaded action should contain only a type prop', function () {
+            const noPayloadAction = functionsCreated['testNoPayload']();
+
+            expect(noPayloadAction.type).to.equal('TEST_NO_PAYLOAD');
+            expect(noPayloadAction.payload).to.be.undefined;
+        });
+
+        it('Payloaded action should contain only a type prop', function () {
+            const noPayloadAction = functionsCreated['testPayloadAll']('test');
+
+            expect(noPayloadAction.type).to.equal('TEST_PAYLOAD_ALL');
+            expect(noPayloadAction.payload).to.equal('test');
+        });
+
+        it('If the configured payload is a function this should be applied', function () {
+            const noPayloadAction = functionsCreated['testPayloadFunction']('test');
+
+            expect(noPayloadAction.type).to.equal('TEST_PAYLOAD_FUNCTION');
+            expect(noPayloadAction.payload).to.equal('test test');
+        });
+    });
+});
